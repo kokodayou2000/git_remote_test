e1dd954a50cc3fe4d925c10c53f27e512aeeb8f6
kokodayou2000 <1611601667@qq.com>
2023-05-14 01:47:23 +0800

    private and  public

--- func.go
+++ func.go
@@ -1,5 +1,20 @@
 package t1
 
-func do(name string) string {
+func Do(name string) string {
+	return doSomeThing(name)
+}
+
+func doSomeThing(name string) string {
+	p := Person{
+		Name: "Tom",
+		age:  18,
+	}
+
+	println(p.Name, p.age)
 	return name + "do do do"
+}
+
+type Person struct {
+	Name string
+	age  int
 }
